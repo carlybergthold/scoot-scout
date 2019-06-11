b9d1b12f5a1c839a11603f735b9ae52f4751a561
Carly Bergthold <carlybergthold@gmail.com>
2019-06-11 11:32:26 -0500

    markers

--- src/components/Map/ScootMap.js
+++ src/components/Map/ScootMap.js
@@ -9,7 +9,7 @@ class ScootMap extends Component {
 
     componentDidMount() {
         // create map
-        const myMap = L.map('map').setView([36, -86], 13);
+        const myMap = L.map('map').setView([36.1218284, -86.7516844], 13);
 
         L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -18,6 +18,18 @@ class ScootMap extends Component {
             accessToken: apiKeys.mapBoxToken
         }
         ).addTo(myMap);
+
+        const scooter = L.marker([36.1218284, -86.7516844]).addTo(myMap).bindPopup("<b>Hello world!</b><br>I am a popup.")
+
+        this.addScootsToMap()
+    }
+
+    addScootsToMap = () => {
+        //creates marker for each scooter location
+        this.props.spins.forEach(scooter => {
+            console.log([scooter.lat, scooter.lon])
+            let marker = L.marker([36.1218284, -86.7516844]).addTo(this.myMap).bindPopup("<b>Hello world!</b><br>I am a popup.")
+        });
     }
 
     render() {
